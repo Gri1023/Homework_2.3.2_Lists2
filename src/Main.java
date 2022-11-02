5c782ddafb1f504ef96844ff4a45ee874b494990
Gri1023 <hryhorii.bavykin@gmail.com>
2022-11-02 19:30:07 +0200

    optimized

--- src/Main.java
+++ src/Main.java
@@ -10,27 +10,28 @@ public class Main
         Scanner scanner = new Scanner(System.in);
         List<String> list = new ArrayList<>();
         int counter = 0;
-        boolean stop = false;
+        boolean stop1 = false;
+        boolean stop2 = false;
         System.out.println ("part 1:");
 
-        while (!scanner.hasNext("!") || !stop)
+        while (!stop1)
         {
             String word = scanner.next();
             if (word.equals("!")) {
-                stop = true;
-                System.out.println("changed to part 2");
+                stop1 = true;
             }
-
-            // part 1
-            else if (!word.equals("!") && !stop) {
+            else if (!word.equals("!") && !stop1) {
                 list.add(word);
             }
-
-            // part 2
-            if (stop) {
-                if (list.contains(word)) {
-                    System.out.println("hit");
-                }
+        }
+        System.out.println ("part 2:");
+        while (!stop2) {
+            String word = scanner.next();
+            if (word.equals("!")) {
+                stop2 = true;
+            }
+            else if (list.contains(word)) {
+                System.out.println("hit");
             }
         }
     }
